3558ba07815db8ecf5a58c9793dc0ca8c09df0cc
Jirattikarn <peepoi_@MacBook-Pro-khxng-Jirattikarn.local>
2020-01-08 14:47:02 +0700

    Random text and image

--- yes_no_project/answers/views.py
+++ yes_no_project/answers/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render 
+from django.http import HttpResponse
+from answers.models import Answer
+
+import random
 
 # Create your views here.
+
+def answer_views(requests):
+    answers = Answer.objects.all()
+    answer = random.choice(answers)
+    return render(requests, 'answer.html', context={'answer': answer})
+
+    
