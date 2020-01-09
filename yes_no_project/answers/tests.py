9ffab5e7507df677b39318e300d91e8b9b15df57
Jirattikarn <peepoi_@MacBook-Pro-khxng-Jirattikarn.local>
2020-01-09 16:51:56 +0700

    [Update] CreateAnswerTest()

--- yes_no_project/answers/tests.py
+++ yes_no_project/answers/tests.py
@@ -1,3 +1,13 @@
 from django.test import TestCase
+from answers.models import Answer
 
-# Create your tests here.
+class CreateAnswerTest(TestCase):
+    def test_create_answer_should_save_model(self):
+        response = self.client.post('/create_answer/add', data={'text':'yes','image':'http://yesno.wtf/api/?force=yes'})
+        actual = Answer.objects.last()
+        expected = {'text' : 'yes','image':'http://yesno.wtf/api/?force=yes'}
+        self.assertEqual(actual.text, expected['text'])
+        self.assertEqual(actual.image, expected['image'])
+
+    def (parameter_list):
+        pass
